ba49e99ab2ebe00b72ba834227c680eba6bae0e6
DMackRus <el16dmcr@leeds.ac.uk>
2024-10-23 16:43:44 +0100

    Small change to run script to auto clone testing repo~

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/TrajOptKP" ]; then
-    
+if [ ! -d "$HOME/MuJoCo_GPU_Learning" ]; then
+    git clone git@github.com:DMackRus/MuJoCo_GPU_Learning.git
 fi
 
 source $HOME/.bashrc
