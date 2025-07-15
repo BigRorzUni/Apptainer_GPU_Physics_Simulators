6f586154dc4c129d50775a0e79da50293f33126d
Rory Edmonds <sc22re@leeds.ac.uk>
2025-07-15 13:49:26 +0100

    newton install fix

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -3,22 +3,22 @@
 if [ ! -d "$HOME/MuJoCo_GPU_Learning" ]; then
     # Skeleton tests
     git clone git@github.com:DMackRus/MuJoCo_GPU_Learning.git
-    
+
     # Create a virtual environment
     python3 -m venv GPU
-    
+
     # Active the venv
     source GPU/bin/activate
 
     pip install --upgrade pip
-    
+
     # Install all python packages into the venv
     pip install matplotlib
     pip install torch torchvision torchaudio 
     pip install pandas
 #    pip install genesis-world
-    
-    
+
+
     # Install mujoco and mujoco viewer
     pip install mujoco
     pip install mujoco-python-viewer
@@ -32,23 +32,25 @@ if [ ! -d "$HOME/MuJoCo_GPU_Learning" ]; then
     # Install mujoco-mjx
     pip install mujoco-mjx
     pip install brax
-    
+
     pip install pytest
 
     #  Warp
-    #pip install uv
-    #uv pip install newton-physics
+    pip install uv
+    uv pip install newton-physics
+
     git clone https://github.com/newton-physics/newton
+    cd newton
     python -m pip install mujoco --pre -f https://py.mujoco.org/
     python -m pip install warp-lang --pre -U -f https://pypi.nvidia.com/warp-lang/
     python -m pip install git+https://github.com/google-deepmind/mujoco_warp.git@main
     python -m pip install -e .[dev]
-
+    cd ..
 else
 
     # Active the venv
     source GPU/bin/activate
-    
+
 fi
 
 source $HOME/.bashrc
