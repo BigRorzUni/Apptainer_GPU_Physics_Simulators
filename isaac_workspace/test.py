f18efa811b75f5fd1d17dcab8e0ec6f2b9210421
RoryEdmonds <123957532+BigRorzUni@users.noreply.github.com>
2025-07-15 11:27:06 +0100

    Delete isaac_workspace directory

--- isaac_workspace/test.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from omni.isaac.kit import SimulationApp
-
-simulation_app = SimulationApp({"headless": True})  # or False if you want GUI
-
-print("Isaac Sim started successfully.")
-
-simulation_app.close()
